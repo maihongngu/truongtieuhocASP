830e3ec011de3b414febc0150c6acbd16214c02e
Ngự Mai <vjpking5913@gmail.com>
2019-09-27 11:08:32 +0700

    Add packages

---
+++ THPT/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using THPT.Models;
+
+namespace THPT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Dizz dm";
+            return View();
+        }
+        public string Contact(string _id)
+        {
+            string message = ("Store.Browse, Genre = " + _id);
+            return message;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ THPT/Controllers/tintucController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace THPT.Controllers
+{
+    public class tintucController : Controller
+    {
+        // GET: /tintuc/       
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+        // GET: /tintuc/Browse       
+        public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+        // GET: /tintuc/Details        
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+    }
+}
